adeb30c74830c023e8ea98e265f7c5a60d731580
FlyTweety <FlyTweety@outlook.com>
2023-07-12 13:17:28 +0800

    s  dnssd

--- dnssd_scanner.py
+++ dnssd_scanner.py
@@ -7,6 +7,8 @@ from scapy.all import raw, DNS, DNSQR
 import utils
 import time
 
+import xml.etree.ElementTree as ET
+
 class DNSSDScanner():
 
     def __init__(self):
@@ -57,7 +59,8 @@ class DNSSDScanner():
         print('[DNS-SD Scanning] Scanning %d locations: %s' % (len(target_ip_list), target_ip_list))
         utils.log('[DNS-SD Scanning] Start')
         utils.log('[DNS-SD Scanning] Scanning %d locations: %s' % (len(target_ip_list), target_ip_list))
-        for target_ip in target_ip_list:
+        for i in range(0, len(target_ip_list)):
+            target_ip = target_ip_list[i]
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             sock.settimeout(5)
 
@@ -68,27 +71,59 @@ class DNSSDScanner():
             try:
                 sock.sendto(raw(req), (target_ip, 5353))
                 data, _ = sock.recvfrom(1024)
+
+                resp = DNS(data)
+                #resp.show()
+
+                print("[DNS-SD Scanning] No.%d %s ONLINE" % (i, target_ip))
+                utils.log("[DNS-SD Scanning] No.%d %s ONLINE" % (i, target_ip))
+                for i in range(0, resp.ancount):
+                    self.get_service_info(sock, target_ip, resp) #在这里面再写known_mdns_info_list
+
             except KeyboardInterrupt:
                 exit(0)
             except:
-                print("[DNS-SD Scanning] %s OFFLINE" % target_ip)
-                utils.log("[DNS-SD Scanning] %s OFFLINE" % target_ip)
+                print("[DNS-SD Scanning] No.%d %s OFFLINE" % (i, target_ip))
+                utils.log("[DNS-SD Scanning] No.%d %s OFFLINE" % (i, target_ip))
                 self.known_mdns_info_list.append({"ip":target_ip, "scan_time":time.time(), "status":False, "services":[]})
-                return
-            resp = DNS(data)
-            #resp.show()
 
-            print("[DNS-SD Scanning] %s ONLINE" % target_ip)
-            utils.log("[DNS-SD Scanning] %s ONLINE" % target_ip)
-            for i in range(0, resp.ancount):
-                self.get_service_info(sock, target_ip, resp) #在这里面再写known_mdns_info_list
+        print('[DNS-SD Scanning] Finish')
+        utils.log('[DNS-SD Scanning] Finish')
 
-            print('[DNS-SD Scanning] Finish')
-            utils.log('[DNS-SD Scanning] Finish')
     # end dnssd_scan()
+
+def getIPs():
+
+    IPs = []
+
+    tree = ET.parse('scan_results.xml')
+    root = tree.getroot()
+
+    # 遍历每个host元素
+    for host in root.findall('host'):
+        # 获取address元素中的addr属性值
+        address = host.find('address')
+        ip_address = address.get('addr')
+        
+        # 打印IP地址
+        IPs.append(ip_address)
+
+    print(len(IPs))
+
+    return IPs
+
+def genIPs(IP):
+
+    IPs = []
+    base = IP.rstrip("0")
+
+    for i in range(1,256):
+        IPs.append(base+str(i))
+
+    return IPs
 
 if __name__ == "__main__":
     DNSSDScannerInstance = DNSSDScanner()
-    target_ip_list = ['192.168.87.48']
+    target_ip_list = getIPs()
     DNSSDScannerInstance.scan(target_ip_list)
 # end main()
