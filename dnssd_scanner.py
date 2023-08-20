f7f6cf24dbb205c896bdd191e59492f8256c16c6
FlyTweety <FlyTweety@outlook.com>
2023-08-20 11:24:00 +0800

    multi an

--- dnssd_scanner.py
+++ dnssd_scanner.py
@@ -11,8 +11,8 @@ class DNSSDScanner():
     def __init__(self):
         self.result_collect = []
 
-    def get_service_info(self, sock, target_ip, resp):
-        service = (resp.an.rdata).decode()
+    def get_service_info(self, sock, target_ip, service):
+        
 
         # query each service detail informations
         req = DNS(id=0x0001, rd=1, qd=DNSQR(qtype="PTR", qname=service))
@@ -23,8 +23,7 @@ class DNSSDScanner():
             data, _ = sock.recvfrom(1024)
             resp = DNS(data)
         except:
-            self.result_collect.append({"ip":target_ip, "scan_time":time.time(), "status":True, "services":['error']})
-            return
+            return []
         #resp.show()
 
         # parse additional records
@@ -46,23 +45,25 @@ class DNSSDScanner():
             else:
                 print(" "*4, rrname)
             services.append(rrname)
-            
-        self.result_collect.append({"ip":target_ip, "scan_time":time.time(), "status":True, "services":services})
+
+        return services
     # end get_service_info()
 
-    # 这个就先不搞异步并发了
-    def scan(self, target_ip_list): #只传ip不传端口，因为默认是5353
+    # no concurrent operations here
+    def scan(self, target_ip_list): # we don't need to specify port here, because we know mDNS uses 5353
+
         print('[DNS-SD Scanning] Start')
         print('[DNS-SD Scanning] Scanning %d locations: %s' % (len(target_ip_list), target_ip_list))
         utils.log('[DNS-SD Scanning] Start')
         utils.log('[DNS-SD Scanning] Scanning %d locations: %s' % (len(target_ip_list), target_ip_list))
+
         for i in range(0, len(target_ip_list)):
             target_ip = target_ip_list[i]
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.settimeout(5)
+            sock.settimeout(3)
 
             # query all service name
-            req = DNS(id=0x0001, rd=1, qd=DNSQR(qtype="PTR", qname="_services._dns-sd._udp.local")) # 这里是关键
+            req = DNS(id=0x0001, rd=1, qd=DNSQR(qtype="PTR", qname="_services._dns-sd._udp.local")) 
             #req.show()
 
             try:
@@ -74,15 +75,21 @@ class DNSSDScanner():
 
                 print("[DNS-SD Scanning] No.%d %s ONLINE" % (i, target_ip))
                 utils.log("[DNS-SD Scanning] No.%d %s ONLINE" % (i, target_ip))
+
+                services = []
                 for i in range(0, resp.ancount):
-                    self.get_service_info(sock, target_ip, resp) #在这里面再写known_mdns_info_list
+                    service = (resp.an[i].rdata).decode()
+                    this_services = self.get_service_info(sock, target_ip, service)
+                    services.append(this_services)
+                self.result_collect.append({"ip":target_ip, "scan_time":time.time(), "status":"ONLINE", "services":services})
+
 
             except KeyboardInterrupt:
                 exit(0)
             except:
                 print("[DNS-SD Scanning] No.%d %s OFFLINE" % (i, target_ip))
                 utils.log("[DNS-SD Scanning] No.%d %s OFFLINE" % (i, target_ip))
-                self.result_collect.append({"ip":target_ip, "scan_time":time.time(), "status":False, "services":[]})
+                self.result_collect.append({"ip":target_ip, "scan_time":time.time(), "status":"OFFLINE", "services":[]})
 
         print('[DNS-SD Scanning] Finish')
         utils.log('[DNS-SD Scanning] Finish')
@@ -95,8 +102,20 @@ class DNSSDScanner():
     def clearResult(self):
         self.result_collect = []
 
+def genIPs(IP):
+
+    IPs = []
+    base = IP.rstrip("0")
+
+    for i in range(1,256):
+        IPs.append(base+str(i))
+
+    return IPs
 
 if __name__ == "__main__":
     DNSSDScannerInstance = DNSSDScanner()
-    DNSSDScannerInstance.scan("127.0.0.1")
+    target_ip_list = genIPs("192.168.87.0")
+    #DNSSDScannerInstance.scan(target_ip_list)
+    DNSSDScannerInstance.scan(['192.168.87.48'])
+    print(DNSSDScannerInstance.getResult())
 # end main()
